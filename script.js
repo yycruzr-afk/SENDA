1afe1f04c9a88851255bfcedb69bb9be477cae83
Yordin_CR <yycruzr@unitru.edu.pe>
2026-07-01 20:55:58 -0500

    Agregacion de modo oscuro y claro

--- script.js
+++ script.js
@@ -29,3 +29,32 @@ btn_hamburgesa.addEventListener('click', () => {
         menuActivo = false;
     }
 })
+
+
+//INTERCAMBIO DE MODOS
+
+let btn_tema = document.querySelector("#btn_cambio");
+
+document.body.classList.add("light-mode");
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (btn_tema) {
+        if (document.body.classList.contains("light-mode")) {
+            btn_tema.textContent = "☀️ Modo Claro";
+        } else {
+            btn_tema.textContent = "🌙 Modo Oscuro";
+        }
+
+        btn_tema.addEventListener("click", () => {
+            document.body.classList.toggle("light-mode");
+
+            if (document.body.classList.contains("light-mode")) {
+                localStorage.setItem("tema", "claro");
+                btn_tema.textContent = "☀️ Modo Claro";
+            } else {
+                localStorage.setItem("tema", "oscuro");
+                btn_tema.textContent = "🌙 Modo Oscuro";
+            }
+        });
+    }
+});
